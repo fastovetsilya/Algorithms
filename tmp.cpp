ddbf7edc4529119b684cc387fefbd6b7a274ccfa
fastovetsilya <ilya.fastovets@gmail.com>
2021-05-22 18:02:13 -0700

    Minor changes

---
+++ tmp.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int division = 52 / 26;
+    int remainder = 52 % 26;
+    cout<<"Division is: "<<division<<endl;
+    cout<<"Remainder is: "<<remainder<<endl;
+    
+    return 0;
+}
